2a162409935ff850240c4015d9abfc52763e1775
mejedi <mejedi@xu-1.mail.msk>
2015-08-18 17:51:13 +0300

    Fix install.sh on Linux

--- install.sh
+++ install.sh
@@ -4,7 +4,15 @@ for path in _*; do
 
 dot_path=$(echo $path | sed -e 's/^_/./')
 
-ln -shfF $(pwd)/$path $HOME/$dot_path
+if [ -h ~/$dot_path ]; then
+rm ~/$dot_path || exit
+fi
+
+if [ -d ~/$dot_path ]; then
+rmdir ~/$dot_path || exit
+fi
+
+ln -sf $(pwd)/$path $HOME/$dot_path || exit
 
 done
 
